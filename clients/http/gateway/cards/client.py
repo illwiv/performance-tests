2e3253135d020e20302e0a583297a0a42d93b3e3
illwiv <13rremezov@gmail.com>
2025-09-21 14:22:14 +0300

    Знакомство с HTTP API клиентами. Практика: Написание HTTP API клиента

---
+++ clients/http/gateway/cards/client.py
@@ -0,0 +1,45 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.http.client import HTTPClient
+
+
+class IssueVirtualCardRequestDict(TypedDict):
+    """
+    Структура данных для создания виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+
+class IssuePhysicalCardRequestDict(TypedDict):
+    """
+    Структура данных для создания физической карты.
+    """
+    userId: str
+    accountId: str
+
+
+class CardsGatewayHTTPClient(HTTPClient):
+    """
+    Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
+    """
+
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequestDict) -> Response:
+        """
+        Создание виртуальной карты.
+
+        :param request: Словарь с данными виртуальной карты.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post(f"/api/v1/cards/issue-virtual-card", json=request)
+
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequestDict) -> Response:
+        """
+        Создание физической карты.
+
+        :param request: Словарь с данными физической карты.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post("/api/v1/cards/issue-physical-card", json=request)

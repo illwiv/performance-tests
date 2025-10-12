961fa63b79985eacefd1904c965dc5e1f0f0a033
illwiv <13rremezov@gmail.com>
2025-10-12 13:46:05 +0300

    Настройки нагрузочных тестов с Pydantic

--- tools/locust/user.py
+++ tools/locust/user.py
@@ -1,10 +1,15 @@
 from locust import User, between
 
+from config import settings
+
 
 class LocustBaseUser(User):
     """
     Пользователь Locust, исполняющий последовательный сценарий получения документов.
     """
     host = "localhost"
-    wait_time = between(1, 3)
+    wait_time = between(
+        min_wait=settings.locust_user.wait_time_min,
+        max_wait=settings.locust_user.wait_time_max
+    )
     abstract = True
